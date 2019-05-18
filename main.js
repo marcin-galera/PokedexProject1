71b7718e75c5729bdfb845ffa61c0a030f3aaebb
marcin-galera <Marcin.galera@outlook.com>
2019-05-18 12:10:53 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+const list = document.querySelector(".list");
+const buttons = document.querySelector(".buttons");
+const details = document.querySelector(".details");
+const imgDetails = document.querySelector(".imgDetails");
+const createImg = document.createElement('img');
+const previousElement = document.querySelector('.previous');
+const nextElement = document.querySelector('.next');
+
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    data.results.map(pokemon => {
+      const liItems = document.createElement("li")
+      liItems.innerText = pokemon.name
+
+      list.append(liItems)
+    })
+
+
+
+    function getNextPokemons() {
+      console.log(data.next)
+      fetch(data.next)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        data.results.map(pokemon => {
+          const liItems = document.createElement("li")
+          liItems.innerText = pokemon.name
+    
+          list.append(liItems)
+      })})
+    }
+
+    nextElement.addEventListener("click", function () {
+      getNextPokemons()
+    })
+
+    // nextElement.addEventListener("click", function () {
+    //   fetch(data.next)
+    //     .then(response => {
+    //       return response.json();
+    //     })
+    //     .then(data => {
+    //       data.results.map(pokemon => {
+    //         const liItems = document.createElement("li")
+    //         liItems.innerText = pokemon.name
+      
+    //         list.append(liItems)
+    //       })
+    //     })
+    // })
+  })
